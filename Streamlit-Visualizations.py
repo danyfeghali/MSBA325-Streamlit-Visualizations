2579d1ec0fb05d75a8e74c78d25c919774ae7ae2
danyfeghali <dselfeghali@gmail.com>
2023-09-16 14:10:15 +0300

    Update Streamlit-Visualizations.py

--- Streamlit-Visualizations.py
+++ Streamlit-Visualizations.py
@@ -21,6 +21,7 @@ st.set_page_config(
 col1, col2, col3 = st.columns([1,3,1])
 with col1:
     st.write("")
+    st.write("")
     st.image('/.world-image-1.jpg', use_column_width = True)
 with col2:
     st.write("")
